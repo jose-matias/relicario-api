59436143b8ce95f61c27f6c5e74d32ed103eac92
Jose Matias <josematias.dev@gmail.com>
2021-10-10 21:00:07 -0300

    feat: add authentication for all routers

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -20,32 +20,32 @@ router.post('/auth/google', passport.authenticate('google-token', { session: fal
 router.post('/auth/facebook', passport.authenticate('facebook-token', { session: false }), AuthController.facebook);
 
 /* User */
-router.post('/user', UserController.store);
-router.get('/user', UserController.index);
+router.post('/user', passportAuth, UserController.store);
+router.get('/user', passportAuth, UserController.index);
 router.get('/user/:id', passportAuth, UserController.show);
 router.put('/user/:id', passportAuth, UserController.update);
 router.delete('/user/:id', passportAuth, UserController.delete);
 
 /* Book */
-router.post('/book', BookController.store);
-router.get('/book', BookController.index);
-router.get('/book/:id', BookController.show);
-router.put('/book/:id', BookController.update);
-router.delete('/book/:id', BookController.delete);
+router.post('/book', passportAuth, BookController.store);
+router.get('/book', passportAuth, BookController.index);
+router.get('/book/:id', passportAuth, BookController.show);
+router.put('/book/:id', passportAuth, BookController.update);
+router.delete('/book/:id', passportAuth, BookController.delete);
 
 /* Author */
-router.post('/author', AuthorController.store);
-router.get('/author', AuthorController.index);
-router.get('/author/:id', AuthorController.show);
-router.put('/author/:id', AuthorController.update);
-router.delete('/author/:id', AuthorController.delete);
+router.post('/author', passportAuth, AuthorController.store);
+router.get('/author', passportAuth, AuthorController.index);
+router.get('/author/:id', passportAuth, AuthorController.show);
+router.put('/author/:id', passportAuth, AuthorController.update);
+router.delete('/author/:id', passportAuth, AuthorController.delete);
 
 /* Category */
-router.post('/category', CategoryController.store);
-router.get('/category', CategoryController.index);
-router.get('/category/:id', CategoryController.show);
-router.put('/category/:id', CategoryController.update);
-router.delete('/category/:id', CategoryController.delete);
+router.post('/category', passportAuth, CategoryController.store);
+router.get('/category', passportAuth, CategoryController.index);
+router.get('/category/:id', passportAuth, CategoryController.show);
+router.put('/category/:id', passportAuth, CategoryController.update);
+router.delete('/category/:id', passportAuth, CategoryController.delete);
 
 /* Files */
 router.post('/file', multer(multerConfig).single('file'), FileController.upload);
