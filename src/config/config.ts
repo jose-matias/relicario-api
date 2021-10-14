f93708ceec9b5be525f807c67fc7cc7189403276
Jose Matias <josematias.dev@gmail.com>
2021-10-13 22:30:33 -0300

    chore: remove credentials for social media

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,9 +1,5 @@
 export default {
   jwtSecret: process.env.JWT_SECRET || 'SecretToken',
-  facebookAppId: process.env.FACEBOOK_APP_ID || '465011334889680',
-  facebookAppSecret: process.env.FACEBOOK_APP_SECRET || '4fc93723119a124fcc0fcd22da80e6a8',
-  googleAppId: process.env.GOOGLE_APP_ID || '598744794483-u5almo77dmnr76bgq3m2te393u8be2mt.apps.googleusercontent.com',
-  googleAppSecret: process.env.GOOGLE_APP_SECRET || 'Isw7nuu__aos0exrN4x7irMH',
   MongoDB: {
     URI: process.env.MONGODB_URI || 'mongodb',
     USER: process.env.MONGODB_USER || 'josematias',
