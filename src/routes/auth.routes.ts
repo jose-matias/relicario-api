8e51f042d7f5532557898d031cd34fa734b27c10
Jose Matias <josematias.dev@gmail.com>
2021-11-17 23:26:15 -0300

    feat: add new routes

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,16 +1,18 @@
-import { Router } from 'express'
-import passport from 'passport'
+import { Router } from 'express';
+import passport from 'passport';
 import multer from 'multer';
-import { multerConfig } from '../config/multer'
+import { multerConfig } from '../config/multer';
 
-import DashController from '../controllers/dash.controller'
-import AuthController from '../controllers/auth.controller'
-import UserController from '../controllers/user.controller'
-import BookController from '../controllers/book.controller'
-import AuthorController from '../controllers/author.controller'
-import CategoryController from '../controllers/category.controller'
-import ReportController from '../controllers/report.controller'
-import FileController from '../controllers/file.controller'
+import DashController from '../controllers/dash.controller';
+import AuthController from '../controllers/auth.controller';
+import UserController from '../controllers/user.controller';
+import BookController from '../controllers/book.controller';
+import AuthorController from '../controllers/author.controller';
+import CategoryController from '../controllers/category.controller';
+import PublishingController from '../controllers/publishing.controller';
+import ReserveController from '../controllers/reserve.controller';
+import ReportController from '../controllers/report.controller';
+import FileController from '../controllers/file.controller';
 
 const router = Router();
 const passportAuth = passport.authenticate("jwt", { session: false });
@@ -48,6 +50,20 @@ router.get('/category', passportAuth, CategoryController.index);
 router.get('/category/:id', passportAuth, CategoryController.show);
 router.put('/category/:id', passportAuth, CategoryController.update);
 router.delete('/category/:id', passportAuth, CategoryController.delete);
+
+/* Publishing */
+router.post('/publishing', passportAuth, PublishingController.store);
+router.get('/publishing', passportAuth, PublishingController.index);
+router.get('/publishing/:id', passportAuth, PublishingController.show);
+router.put('/publishing/:id', passportAuth, PublishingController.update);
+router.delete('/publishing/:id', passportAuth, PublishingController.delete);
+
+/* Reserve */
+router.post('/reserve', passportAuth, ReserveController.store);
+router.get('/reserve', passportAuth, ReserveController.index);
+router.get('/reserve/:id', passportAuth, ReserveController.show);
+router.put('/reserve/:id', passportAuth, ReserveController.update);
+router.delete('/reserve/:id', passportAuth, ReserveController.delete);
 
 /* Files */
 router.post('/file', multer(multerConfig).single('file'), FileController.upload);
