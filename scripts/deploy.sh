80f3c965a928d71b747482d20f96a415c53ddd42
José Matias <josematias.dev@gmail.com>
2022-03-03 20:27:40 -0300

    chore: test change key

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 mkdir -p ~/.ssh
-echo -e "${SSH_KEY}" > ~/.ssh/id_rsa
-chmod 600 ~/.ssh/id_rsa
+echo -e "${SSH_KEY}" > ~/.ssh/id_ed25519
+chmod 600 ~/.ssh/id_ed25519
 
-ssh-keyscan -p 22 -t rsa josematias.dev 2>&1 >> ~/.ssh/known_hosts
+ssh-keyscan -p 22 -t ed25519 josematias.dev 2>&1 >> ~/.ssh/known_hosts
