bd9848454cbe3370d3b4449b6528f386dbe28df5
José Matias <josematias.dev@gmail.com>
2022-03-03 23:47:39 -0300

    chore: test git actions

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,3 +3,5 @@ echo -e "${SSH_KEY}" > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 
 ssh-keyscan -H ${SSH_HOST} >> ~/.ssh/known_hosts
+
+rsync -avz ./dist/ ${SSH_USER}@${SSH_HOST}:/home/backend/relicario-api/dist/
