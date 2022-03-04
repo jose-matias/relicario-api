7b430f0fd3d3dee2278f6cc9b317598d89c5ba52
José Matias <josematias.dev@gmail.com>
2022-03-03 23:34:04 -0300

    chore: test git actions

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,4 +2,4 @@ mkdir -p ~/.ssh
 echo -e "${SSH_KEY}" > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 
-ssh-keyscan -H josematias.dev >> ~/.ssh/known_hosts
+ssh-keyscan -H ${{ secrets.SSH_HOST }} >> ~/.ssh/known_hosts
