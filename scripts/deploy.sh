af47110ceb711d9467a351034a4d02312c9ac5e3
José Matias <josematias.dev@gmail.com>
2022-03-07 22:03:32 -0300

    chore: test git actions

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,7 +5,3 @@ chmod 600 ~/.ssh/id_rsa
 ssh-keyscan -H ${SSH_HOST} >> ~/.ssh/known_hosts
 
 export PATH=$PATH:/usr/bin
-
-which rsync || ( apt-get update -y && apt-get install rsync -y )
-
-rsync -avz ./dist/ ${SSH_USER}@${SSH_HOST}:/home/backend/relicario-api/dist/
