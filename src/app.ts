2d49ef78591f0c0f8d9f377f4616a0abfae082ee
José Matias <josematias.dev@gmail.com>
2022-04-19 21:38:17 -0300

    fix: cors origin error

--- src/app.ts
+++ src/app.ts
@@ -17,20 +17,7 @@ const app = express();
 app.set('port', process.env.SERVER_PORT || 3700);
 
 // Middlewares
-// app.use(cors());
-app.use((req, res, next) => {
-  const corsWhitelist = [
-    'https://relicario.josematias.dev/',
-  ];
-
-  if (req.headers.origin && corsWhitelist.indexOf(req.headers.origin) !== -1) {
-    res.header('Access-Control-Allow-Origin', req.headers.origin);
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  }
-
-  next();
-});
-
+app.use(cors());
 
 // app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: false }));
