cc359e7313cce9e6d31697bbd1496f8ed937128c
José Matias <josematias.dev@gmail.com>
2022-04-23 01:15:42 -0300

    feat: add new fields in dashboard info

--- src/controllers/dash.controller.ts
+++ src/controllers/dash.controller.ts
@@ -1,45 +1,39 @@
 import { Request, Response } from 'express';
+import { startOfDay, endOfDay } from 'date-fns';
 
 import User from '../models/user';
 import Book from '../models/book';
-import Category from '../models/category';
 import Author from '../models/author';
-import moment from 'moment';
-import { startOfDay, endOfDay } from 'date-fns';
+import Category from '../models/category';
+import Publisher from '../models/publisher';
+import Suggestion from '../models/suggestion';
+import Reserve from '../models/reserve';
 
 class DashController {
   async index(req: Request, res: Response) {
     try {
-      const users = await User.countDocuments();
+      const activeUsers = await User.countDocuments({ status: true });
+      const inactiveUsers = await User.countDocuments({ status: false });
       const books = await Book.countDocuments();
-      const categories = await Category.countDocuments();
       const authors = await Author.countDocuments();
-
-      const dates = [];
-      const date = new Date();
-
-      for (var i = 0; i < 7; i++){
-        var tempDate = new Date();
-        tempDate.setDate(date.getDate() - i);
-        const start = startOfDay(tempDate);
-        const end = endOfDay(tempDate);
-
-        const booksByDay = await Book.countDocuments({
-          createdAt: {
-            $gte: start,
-            $lte: end,
-          }
-        });
-
-        dates.push({ date: start, books: booksByDay });
-      }
+      const categories = await Category.countDocuments();
+      const publishers = await Publisher.countDocuments();
+      const suggestions = await Suggestion.countDocuments();
+      const suggestionReview = await Suggestion.countDocuments({ status: true });
+      const reserves = await Reserve.countDocuments();
+      const confirmateReserves = await Reserve.countDocuments({ status: 'Reservado' });
 
       return res.json({
-        users,
+        activeUsers,
+        inactiveUsers,
         books,
-        categories,
         authors,
-        lastAddedBooks: dates,
+        categories,
+        publishers,
+        suggestions,
+        suggestionReview,
+        reserves,
+        confirmateReserves,
       });
     } catch (error) {
       return res.status(500).json(error);
